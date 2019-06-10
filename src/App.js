e3d31d865a1e75a67425b3e5dcf5e040491a0d2d
Scott Schindler <scottschindler29@gmail.com>
2019-06-09 22:11:42 -0400

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => (
   <div>
+    <Router>
+
+    <Route exact path ="/" component = {Home}/>
+    <Route path ="/about" component = {About}/>
+    <Route path ="/contact" component = {Contact}/>
     <Navigation />
+    </Router>
   </div>
 );
 
 export default App;
+
+
+// Create 3 <Route /> setting their path prop equal to /, /about, /contact with their respective components.
